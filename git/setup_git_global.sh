9aad330f138af95a5d08d73b2f277955419555fe
Jun Ren <jun.ren.nz@gmail.com>
2022-11-21 06:56:07 +1300

    updated nvim config

--- git/setup_git_global.sh
+++ git/setup_git_global.sh
@@ -1,28 +1,9 @@
 #! /bin/bash
 
-# install flake8
-pip install flake8
-
 # link global flake8 config
 if [ ! -d "~/.config/" ]; then
     mkdir ~/.config/
 fi
-
-if [ ! -d "~/.git_hooks/" ]; then
-    mkdir ~/.git_hooks/
-fi
-
-# Due to constant confusion by users, user-level Flake8 configuration files are no longer supported. 
-ln -sf $HOME/local_install/configs/git/flake8 ~/.config/flake8
-
-pip install isort
-ln -sf $HOME/local_install/configs/git/isort.cfg ~/.isort.cfg
-
-if [ ! -d "~/.git_hooks/" ]; then
-    mkdir ~/.git_hooks/
-fi
-# link pre-commit hook into global commit hook directory
-ln -sf $HOME/local_install/configs/git/pre-commit ~/.git_hooks/pre-commit
 
 # link global git ignore
 ln -sf $HOME/local_install/configs/git/git_ignore_global ~/.gitignore_global
